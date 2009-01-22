e00de3ac0b5ff1600131a4b058cd06919336de57
Kaity G. B <uberchick@uberchicgeekchick.com>
2009-01-22 04:46:40 -0700

    I added supported to 'gPodder:Continue.pl' so it now supports an argument of an integer of how many times the interupt signal is sent to all of gpodder's PID

--- media-organization/gPodder/gPodder:Continue.pl
+++ media-organization/gPodder/gPodder:Continue.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
 use strict;
-
-foreach(`pidof -x gpodder`){`kill -INT $_`;}
+my $total=2;
+if($ARGV[0]>0){$total=$ARGV[0];}
+print("Sending Interupt signal to gPodder's PIDs:");
+foreach(`pidof -x gpodder`){print("\nInterupting $_"); for(my $i=0; $i<$total; $i++){print(".");`kill -INT $_`; sleep(2);}}

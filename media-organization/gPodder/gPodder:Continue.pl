c04e4f1ee896e18f0c6865e3e0d496aedc21371b
Kaity G. B <uberchick@uberchicgeekchick.com>
2009-01-22 04:52:19 -0700

    I added supported to 'gPodder:Continue.pl' so it now supports an argument of an integer of how many times the interupt signal is sent to all of gpodder's PIDs.  It will send INT to each PID at least twice.

--- media-organization/gPodder/gPodder:Continue.pl
+++ media-organization/gPodder/gPodder:Continue.pl
@@ -2,5 +2,13 @@
 use strict;
 my $total=2;
 if($ARGV[0]>0){$total=$ARGV[0];}
-print("Sending Interupt signal to gPodder's PIDs:");
-foreach(`pidof -x gpodder`){print("\nInterupting $_"); for(my $i=0; $i<$total; $i++){print(".");`kill -INT $_`; sleep(2);}}
+print("Sending Interupt signal to gPodder's PIDs");
+foreach(`pidof -x gpodder`){
+	print("Interupting $_");
+	for(my $i=0; $i<$total; $i++){
+		print(".");
+		`kill -INT $_`;
+		sleep(2);
+	}
+	print("\n");
+}

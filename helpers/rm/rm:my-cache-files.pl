f0220c2e2eb37b03fbde62fcf492896e88956609
Kaity G. B <uberchick@uberchicgeekchick.com>
2010-05-18 20:11:20 -0600

    2010.05.18
    	Still more bug fixes, improvements, & refactoring of my playlist-manager, [media/playlist-manager/], scripts.
    
    	I've also extended my tcsh custom signal & interupt handling; though I still to implement it many of many other scripts.
    
    	I've also fixed my sudo alias command login script.
    
    	I've also straightened out my priorities & they're now reflected in my gnome-terminal launcher [devel/my:artistic:ide].
    
    Committed on : 2010-05-18 @ 08:11:15 PM

--- helpers/rm/rm:my-cache-files.pl
+++ helpers/rm/rm:my-cache-files.pl
@@ -29,13 +29,41 @@ sub var_escape_for_shell{
 		$search_dir="$ARGV[0]";
 	}
 	
-	my $seconds=`date '+%s'`;
-	chomp($seconds);
-	my $rm_lst="$search_dir/.rm.$seconds.lst";
-	system("tcsh -f -c '(find -L \"".var_escape_for_shell($search_dir, $FALSE)."\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/.(oggconvert|tcsh\-script\.template|tcsh\-template|perl\-script\.template|perl\-template|filelist|filenames|alacast|my\.feed|gPodder|New|rm|argument|escaped).*\" > \"$rm_lst\" ) >& /dev/null;'");
+	my $rm_lst=`mktemp --tmpdir .rm.my-cache-files.lst.XXXXXX`;
+	chomp($rm_lst);
+	system("tcsh -f -c '(find -L \"".var_escape_for_shell($search_dir, $FALSE)."\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/tmpdir\.for\..*\" -type -d | sed -r \'\\\'\'s/(.*)/if(\ \-d \"\\1\"\ ) rm \-rv\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+	system("tcsh -f -c '(find -L \"".var_escape_for_shell($search_dir, $FALSE)."\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/.(oggconvert|tcsh\-script\.template|tcsh\-template|perl\-script\.template|perl\-template|filelist|filenames|alacast|my\.feed|gPodder|New|rm|argument|escaped).*\" | sed -r \'\\\'\'s/(.*)/\\tif(\ \-e \"\\1\"\ ) rm \-v\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+	
+	if( -d "/tmp" ){
+		system("tcsh -f -c '(find -L \"/tmp\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/tmpdir\.for\..*\" -type d | sed -r \'\\\'\'s/(.*)/if(\ \-d \"\\1\"\ ) rm \-rv\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+		system("tcsh -f -c '(find -L \"/tmp\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/.(oggconvert|tcsh\-script\.template|tcsh\-template|perl\-script\.template|perl\-template|filelist|filenames|alacast|my\.feed|gPodder|New|rm|argument|escaped).*\" | sed -r \'\\\'\'s/(.*)/\\tif(\ \-e \"\\1\"\ ) rm \-v\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+	}
+	
+	if( -d "$ENV{HOME}/tmp" ){
+		system("tcsh -f -c '(find -L \"$ENV{HOME}/tmp\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/tmpdir\.for\..*\" -type d | sed -r \'\\\'\'s/(.*)/if(\ \-d \"\\1\"\ ) rm \-rv\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+		system("tcsh -f -c '(find -L \"$ENV{HOME}/tmp\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/.(oggconvert|tcsh\-script\.template|tcsh\-template|perl\-script\.template|perl\-template|filelist|filenames|alacast|my\.feed|gPodder|New|rm|argument|escaped).*\" | sed -r \'\\\'\'s/(.*)/\\tif(\ \-e \"\\1\"\ ) rm \-v\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+	}
+	
+	if( "$ENV{TMPDIR}"ne"" && "$ENV{TMPDIR}"ne"/tmp" && "$ENV{TMPDIR}"ne"$ENV{HOME}/tmp" && -d "$ENV{TMPDIR}" ){
+		system("tcsh -f -c '(find -L \"$ENV{TMPDIR}\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/tmpdir\.for\..*\" -type d | sed -r \'\\\'\'s/(.*)/if(\ \-d \"\\1\"\ ) rm \-rv\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+		system("tcsh -f -c '(find -L \"$ENV{TMPDIR}\" -ignore_readdir_race -regextype posix-extended -iregex \".*\/.(oggconvert|tcsh\-script\.template|tcsh\-template|perl\-script\.template|perl\-template|filelist|filenames|alacast|my\.feed|gPodder|New|rm|argument|escaped).*\" | sed -r \'\\\'\'s/(.*)/\\tif(\ \-e \"\\1\"\ ) rm \-v\ \"\\1\";/\'\\\'\' >> \"$rm_lst\" ) >& /dev/null;'");
+	}
+	
 	my @files=`cat "$rm_lst"`;
 	if( @files > 0 ) {
-		system("tcsh -f -c 'rm -v \"`cat \"$rm_lst\"`\";'");
+		my $tcsh_script="$rm_lst.tcsh";
+		open(tcsh_script, ">", "$tcsh_script");
+		open(rm_lst, "<", "$rm_lst");
+		print tcsh_script "#!/bin/tcsh -f\n";
+		while(my $line = <rm_lst>){
+			print tcsh_script "$line";
+		}
+		print tcsh_script "rm -v $tcsh_script\n";
+		close(rm_lst);
+		close(tcsh_script);
+		chmod(0777, $tcsh_script);
+		system("tcsh -f -c '$ENV{EDITOR} $tcsh_script;'");
+		system("tcsh -f -c '$tcsh_script;'");
 	}
 	
 	if( -e "$rm_lst" ) {
